8fd9b6ede4a485e75dc1e81b4946075ecdf5e438
Ali <62906333+ali1987isaev@users.noreply.github.com>
2020-08-17 15:22:58 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,18 +1,37 @@
-$('.gallery__images, .instagram__slider').slick({
-  slidesToShow: 4,
-  slidesToScroll: 1,
-  autoplay: true,
-  autoplaySpeed: 2000,
-});
-
-document.querySelectorAll('.slick-arrow').forEach(arrow => arrow.remove());
-
+const isMobile = {
+	Android: function() {return navigator.userAgent.match(/Android/i);},
+	BlackBerry: function() {return navigator.userAgent.match(/BlackBerry/i);},
+	iOS: function() {return navigator.userAgent.match(/iPhone|iPad|iPod/i);},
+	Opera: function() {return navigator.userAgent.match(/Opera Mini/i);},
+	Windows: function() {return navigator.userAgent.match(/IEMobile/i);},
+	any: function() {return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());}
+};
 
 const form = document.querySelector('[data-form-email]');
 const email = document.querySelector('[data-email-input]');
 const burger = document.querySelector('.header__burger');
 const menu = document.querySelector('.menu');
 const body = document.querySelector('body');
+
+if(isMobile.any()){
+  body.classList.add('touch');
+  $('.gallery__images, .instagram__slider').slick({
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  document.querySelectorAll('.slick-arrow').forEach(arrow => arrow.remove());
+}else{
+  body.classList.add('mouse');
+  $('.gallery__images, .instagram__slider').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
+  document.querySelectorAll('.slick-arrow').forEach(arrow => arrow.remove());
+}
 
 burger.addEventListener('click', e=> {
   burger.classList.toggle('active');
