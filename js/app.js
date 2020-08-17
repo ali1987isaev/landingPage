42e5247778f7e64d6bb02df1843d284553784ee7
Ali <62906333+ali1987isaev@users.noreply.github.com>
2020-08-17 16:36:38 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -13,25 +13,19 @@ const burger = document.querySelector('.header__burger');
 const menu = document.querySelector('.menu');
 const body = document.querySelector('body');
 
-if(isMobile.any()){
-  body.classList.add('touch');
-  $('.gallery__images, .instagram__slider').slick({
+isMobile.any()
+  ? $('.gallery__images, .instagram__slider').slick({
     slidesToShow: 2,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
-  });
-  document.querySelectorAll('.slick-arrow').forEach(arrow => arrow.remove());
-}else{
-  body.classList.add('mouse');
-  $('.gallery__images, .instagram__slider').slick({
+    autoplaySpeed: 2000})
+  : $('.gallery__images, .instagram__slider').slick({
     slidesToShow: 4,
     slidesToScroll: 1,
     autoplay: true,
-    autoplaySpeed: 2000,
-  });
-  document.querySelectorAll('.slick-arrow').forEach(arrow => arrow.remove());
-}
+    autoplaySpeed: 2000});
+
+document.querySelectorAll('.slick-arrow').forEach(arrow => arrow.remove());
 
 burger.addEventListener('click', e=> {
   burger.classList.toggle('active');
